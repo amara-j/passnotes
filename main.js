9b9d1a888ef44b9c514fa1ab1bf6312325902781
azhadsyed <azhadsyed17@gmail.com>
2020-12-03 12:25:31 -0500

    playback now plays in time, added black keys

--- main.js
+++ main.js
@@ -2,91 +2,63 @@ let prompt = []
 
 const synth = new Tone.Synth().toDestination();
 
-function playRiff() {
+function play() {
     Tone.start()
-    const seq = new Tone.Sequence((time, note) => {
-        synth.triggerAttackRelease(note, 0.1, time);
-        // subdivisions are given as subarrays
-    }, prompt).start();
-    Tone.Transport.start();
+    for (let i = 0; i < prompt.length; i++) {
+        setTimeout(function () {
+            synth.triggerAttackRelease(prompt[i][1], 0.1)
+        }, prompt[i][0]);
+    }
 
-    /*
-    var seq = new Tone.Sequence((time, note) => {
-        synth.triggerAttackRelease(note, 0.1, time);
-        [null, ["C5", "C5"], [null, "C5"], [null, "C5"], "B4", "D5", "C5", "G4"]
-    }).start(0);
-    */
-
-    seq.loop = false
-
-    Tone.Transport.start()
 }
-
-// [null, ["C5", "C5"], [null, "C5"], [null, "C5"], "B4", "D5", "C5", "G4"] Anda da see
 
 asciToNote = {
     "KeyA": "C4",
+    "KeyW": "C#4",
     "KeyS": "D4",
+    "KeyE": "D#4",
     "KeyD": "E4",
     "KeyF": "F4",
+    "KeyT": "F#4",
     "KeyG": "G4",
+    "KeyY": "G#4",
     "KeyH": "A4",
+    "KeyU": "A#4",
     "KeyJ": "B4",
     "KeyK": "C5",
+    "KeyO": "C#5",
     "KeyL": "D5",
     "Semicolon": "E5"
 }
 
-let isRecording = 0
+let timeElapsed = 0;
+let isRecording = 0;
 
 function record() {
+    timeElapsed = 0
     isRecording = 1
+    prompt = []
 }
 
 function stop() {
     isRecording = 0
-    console.log(prompt)
+    console.log(processPerformance(prompt))
 }
 
 document.addEventListener('keydown', function (e) {
     let note = asciToNote[e.code];
     if (note && isRecording === 1) {
         synth.triggerAttackRelease(note, 0.1)
-        prompt.push(note)
-        // play it or somth
+        prompt.push([Date.now(), note])
     }
-    //console.log(asciToNote[e.code])
 });
 
 
-
-
-
-
-// prompt
-
-// press the "record prompt" button
-// press any key to begin recording
-// press record again/stop to stop recording
-// yes/no buttons: do you want to keep this as the prompt?
-    // - if yes: continue, if no, return to record step
-
-// response
-
-// press the "record response" button
-// press any key to begin recording 
-// stops recording after sufficient time has passed since last key press
-// user is prompted to enter their password again
-// check for similarity
-//      if not similar:
-        //ask to reverify OR rewrite
-        //else: move to the note page
-
-// note
-
-// give user space to write on a tacky-ass yellow notepad/sticky
-// save button to save
-// now, they would have to navigate to the front page, find their prompt and enter the password to see their note.
-
-
-        //write prompt/password/sticky note combination to the data layer
+function processPerformance(array) {
+    let subtractMe = array[0][0]
+    let relativeArray = array
+    for (let i = 0; i < array.length; i++) {
+        relativeArray[i][0] -= subtractMe
+    }
+    return relativeArray
+}
