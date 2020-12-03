613c19fce35e38cf4b758fc0866ac584a39fe21a
Amara Jaeger <amara@Amaras-MacBook-Air.local>
2020-12-02 22:49:22 -0500

    can now record and play back a riff! play synth notes from qwerty keys in real time.  next steps: record rhythm as well as pitch

--- main.js
+++ main.js
@@ -1,24 +1,92 @@
+let prompt = []
+
+const synth = new Tone.Synth().toDestination();
+
 function playRiff() {
     Tone.start()
-    const synth = new Tone.Synth().toDestination();
     const seq = new Tone.Sequence((time, note) => {
         synth.triggerAttackRelease(note, 0.1, time);
         // subdivisions are given as subarrays
-    }, [null, ["C5", "C5"], [null, "C5"], [null, "C5"], "B4", "D5", "C5", "G4"]).start(0);
+    }, prompt).start();
+    Tone.Transport.start();
+
+    /*
+    var seq = new Tone.Sequence((time, note) => {
+        synth.triggerAttackRelease(note, 0.1, time);
+        [null, ["C5", "C5"], [null, "C5"], [null, "C5"], "B4", "D5", "C5", "G4"]
+    }).start(0);
+    */
 
     seq.loop = false
-
-    // loopA.probabilityy = 1;
 
     Tone.Transport.start()
 }
 
-asciToNote = { 97: "C4", 115: "D4", 100: "E4", 102: "F4", 103: "G4", 104: "A4", 106: "B4", 107: "C4", 108: "D5", 59: "E5" }
+// [null, ["C5", "C5"], [null, "C5"], [null, "C5"], "B4", "D5", "C5", "G4"] Anda da see
 
-document.addEventListener("keydown", function (event) {
-    let asci = event.keyCode
-    if (asci === 97) {
-        console.log("you played a")
-        //console.log(asciToNote[asci]);
+asciToNote = {
+    "KeyA": "C4",
+    "KeyS": "D4",
+    "KeyD": "E4",
+    "KeyF": "F4",
+    "KeyG": "G4",
+    "KeyH": "A4",
+    "KeyJ": "B4",
+    "KeyK": "C5",
+    "KeyL": "D5",
+    "Semicolon": "E5"
+}
+
+let isRecording = 0
+
+function record() {
+    isRecording = 1
+}
+
+function stop() {
+    isRecording = 0
+    console.log(prompt)
+}
+
+document.addEventListener('keydown', function (e) {
+    let note = asciToNote[e.code];
+    if (note && isRecording === 1) {
+        synth.triggerAttackRelease(note, 0.1)
+        prompt.push(note)
+        // play it or somth
     }
+    //console.log(asciToNote[e.code])
 });
+
+
+
+
+
+
+// prompt
+
+// press the "record prompt" button
+// press any key to begin recording
+// press record again/stop to stop recording
+// yes/no buttons: do you want to keep this as the prompt?
+    // - if yes: continue, if no, return to record step
+
+// response
+
+// press the "record response" button
+// press any key to begin recording 
+// stops recording after sufficient time has passed since last key press
+// user is prompted to enter their password again
+// check for similarity
+//      if not similar:
+        //ask to reverify OR rewrite
+        //else: move to the note page
+
+// note
+
+// give user space to write on a tacky-ass yellow notepad/sticky
+// save button to save
+// now, they would have to navigate to the front page, find their prompt and enter the password to see their note.
+
+
+        //write prompt/password/sticky note combination to the data layer
