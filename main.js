cbc63c000369974991d62d46449abfcc454cd045
Amara Jaeger <amara@Amaras-MacBook-Air.local>
2020-12-08 14:44:22 -0500

    all front end is working and abstracted

--- main.js
+++ main.js
@@ -1,42 +1,21 @@
-const backBeatThreshold = 500; // amount of rhythmic difference (millisec) to allow 
-const continueTextArray = ["", "Record as many times as you like until you're happy with your take.", "Press continue to save your take.", ""]
-const synth = new Tone.Synth().toDestination();
-const asciToNote = {
-    "KeyA": "C4",
-    "KeyW": "C#4",
-    "KeyS": "D4",
-    "KeyE": "D#4",
-    "KeyD": "E4",
-    "KeyF": "F4",
-    "KeyT": "F#4",
-    "KeyG": "G4",
-    "KeyY": "G#4",
-    "KeyH": "A4",
-    "KeyU": "A#4",
-    "KeyJ": "B4",
-    "KeyK": "C5",
-    "KeyO": "C#5",
-    "KeyL": "D5",
-    "Semicolon": "E5"
-}
-
-let prompt = []
-let take1 = []
-let take2 = []
-let authStep = 1
-let timeElapsed = 0;
-let isRecording = 0;
-
-
 function record(authStep, recordArray) {
     authStepString = authStep.toString()
     showElement(`stopButton${authStepString}`)
     timeElapsed = 0
     isRecording = 1
-    recordArray = []
+    recordArray.length = 0
 }
 
-function recordHelper(array, note) {array.push([Date.now(), note])}
+function stop(authStep, recordArray) {
+    authStepString = authStep.toString()
+    isRecording = 0;
+    if (recordArray.length > 0) {
+        recordArray = processPerformance(recordArray)
+        showElement(`playButton${authStepString}`)
+        showElement(`continueButton${authStepString}`)
+        updateText(`step${authStepString}Instructions`, continueTextArray[authStepString])
+    }
+}
 
 function processPerformance(array) {
     let subtractMe = array[0][0]
@@ -47,19 +26,7 @@ function processPerformance(array) {
     return relativeArray
 }
 
-function stop(authStep, recordArray) {
-    authStepString = authStep.toString()
-    isRecording = 0;
-    if (recordArray.length > 0) {
-        processPerformance(recordArray)
-        showElement(`playButton${authStepString}`)
-        showElement(`continueButton${authStepString}`)
-        updateText(`step${authStepString}Instructions`, continueTextArray[authStepString])
-    }
-}
-
 function play(array) {
-    console.log(array)
     Tone.start()
     for (let i = 0; i < array.length; i++) {
         setTimeout(function () {
@@ -67,6 +34,8 @@ function play(array) {
         }, array[i][0]);
     }
 }
+
+function recordHelper(array, note) { array.push([Date.now(), note]) }
 
 
 function recordHelper(array, note) {
@@ -80,16 +49,13 @@ document.addEventListener('keydown', function (e) {
         synth.triggerAttackRelease(note, 0.1)
     }
     if (note && isRecording === 1 && authStep === 1) {
-        recordHelper(prompt, note)
-        console.log("pushing to prompt")
+        recordHelper(take0, note)
     }
     if (note && isRecording === 1 && authStep === 2) {
         recordHelper(take1, note)
-        console.log("pushing to take1")
     }
     if (note && isRecording === 1 && authStep === 3) {
         recordHelper(take2, note)
-        console.log("pushing to take2")
     }
 });
 
@@ -114,7 +80,7 @@ function highlightDown(element) {
     document.getElementById(element).style.opacity = 0.1;
 }
 
-function keepPrompt() {
+function keepTake0() {
     authStep++
     hideElement("step1Instructions")
     hideElement("recordButton1")
@@ -137,7 +103,6 @@ function keepTake1() {
     hideElement("step2Instructions")
     hideElement("continueButton2")
     highlightUp("step3Text")
-    showElement("step3Instructions")
     showElement("recordButton3")
 }
 
@@ -146,8 +111,7 @@ function keepTake2() {
     hideElement("recordButton3")
     hideElement("stopButton3")
     hideElement("playButton3")
-    // highlightDown("step3Text")
-    hideElement("authButton")
+    hideElement("continueButton3")
     console.log("AUTHENTICATION TRIGGERED")
     tryAuthentication(take1, take2)
 }
@@ -156,7 +120,6 @@ function tryAuthentication(take1, take2) {
     hideElement("step1Text")
     hideElement("step2Text")
     hideElement("step3Text")
-    hideElement("authButton")
     if (authenticate(take1, take2)) {
         authenticationStep++
         document.body.style.backgroundColor = "lightSkyBlue";
