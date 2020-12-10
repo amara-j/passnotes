4981ff31ff88c2f42ae520964e866b28f50915ee
azhadsyed <azhadsyed17@gmail.com>
2020-12-10 12:32:39 -0500

    bugfix - content now saving to db

--- main.js
+++ main.js
@@ -162,7 +162,7 @@ function getNotes() {
 function saveNote() {
     const requestBody = {
         title: "Test Title",
-        content: document.getElementById('saveNote').value,
+        content: document.getElementById('note').value,
         prompt: take0,
         password: take1
     }
