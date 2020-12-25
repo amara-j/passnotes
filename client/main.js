631bcf8cf4adf1099d0d5fd57adc5da4a5035419
azhadsyed <azhadsyed17@gmail.com>
2020-12-24 20:31:20 -0500

    keyboard makes noise and lights up

--- client/main.js
+++ client/main.js
@@ -1,4 +1,12 @@
-//https://1000mileworld.com/Portfolio/Piano/keyboard.html
+/*
+https://1000mileworld.com/Portfolio/Piano/keyboard.html
+
+TODO:
+1. (x) Hear sound on keypress
+2. Light up on keypress
+3. Record mechanism
+4. Working I/O
+*/
 
 const keycodeToNote = {
     "KeyA": "C4",
@@ -49,24 +57,56 @@ const notesInOrder = [
 
 var iWhite = 0
 
-var keyboard = document.createElement("div")
-keyboard.ClassName = "keyboard"
+var keyboard = document.getElementById("keyboard")
 
 for (var n in notesInOrder) {
     var thisNote = notesInOrder[n]
     var thisKey = document.createElement("div")
     if (thisNote.length > 2) {
-        var attrs = ["black key", "30px", "120px", 40 * iWhite - 15 + "px"]
+        var attrs = ["black key", "30px", "120px", 40 * iWhite - 15 + "px", "black"]
     } else {
-        var attrs = ["white key", "40px", "200px", 40 * iWhite + "px"]
+        var attrs = ["white key", "40px", "200px", 40 * iWhite + "px", "white"]
         iWhite++
     }
     thisKey.className = attrs[0]
     thisKey.style.width = attrs[1]
     thisKey.style.height = attrs[2]
     thisKey.style.left = attrs[3]
+    thisKey.style.backgroundColor = attrs[4]
+    thisKey.setAttribute("id", thisNote)
 
     keyboard.appendChild(thisKey)
 }
 
-document.body.appendChild(keyboard)
+keyboard.style.width = 40 * 10 + "px"
+keyboard.style.height = 200 + "px"
+
+const synth = new Tone.Synth().toDestination()
+
+document.addEventListener('keydown', (e) => {
+    let note = keycodeToNote[e.code];
+    if (note) {
+        synth.triggerAttackRelease(note, 0.1)
+        lightKeyboard("down", note)
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    let note = keycodeToNote[e.code];
+    if (note) {
+        lightKeyboard("up", note)
+    }
+});
+
+const lightKeyboard = (event, note) => {
+    if (note.length > 2) {
+        var upColor = "black"
+    } else {
+        var upColor = "white"
+    }
+    if (event === "down") {
+        document.getElementById(note).style.backgroundColor = "purple"
+    } else {
+        document.getElementById(note).style.backgroundColor = upColor
+    }
+}
