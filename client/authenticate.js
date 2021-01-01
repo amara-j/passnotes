cf91fed55e0d0ba2a2f1dd3452301c17d6d80375
azhadsyed <azhadsyed17@gmail.com>
2021-01-01 13:59:16 -0500

    compositive refactor

--- client/authenticate.js
+++ client/authenticate.js
@@ -1,44 +1,45 @@
-const backBeatThreshold = 500; // amount of rhythmic difference (millisec) to allow 
-// should we somehow be accounting for time stretches/tempo differences? :/
-
 //function to compute the difference between two performances
-function authenticate(original, take2,) {
-    // first test: are the two arrays the same length:
-    let sameLength = original.length === take2.length;
-    let sameNotes = false;
-    let sameRhythm = false;
-    if (sameLength) { // if length is the same, check that each note is the same
-        for (let i = 0; i < original.length; i++) {
-            if (original[i][1] != take2[i][1]) {
-                sameNotes = false
-                break
-            } else {
-                sameNotes = true
-            }
+
+const compareLength = (a, b) => {
+    return a.length === b.length
+}
+
+const compareNotes = (a, b) => {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i][1] != b[i][1]) {
+            return false
         }
-    }
-    if (sameLength && sameNotes) { // if length and each note are the same, check rhythm
-        for (let i = 0; i < original.length; i++) {
-            if (Math.abs(original[i][0] - take2[i][0]) > backBeatThreshold) {
-                sameRhythm = false
-                break
-            } else {
-                sameRhythm = true
-            }
-        }
-        console.log("same length:", sameLength)
-        console.log("same notes:", sameNotes)
-        console.log("same rhythm:", sameRhythm)
-        // return sameLength, sameNotes, sameRhythm
-    }
-    if (sameLength && sameNotes && sameRhythm) {
         return true
-    } {
+    }
+}
+
+const compareRhythm = (a, b, threshold) => {
+    for (let i = 0; i < a.length; i++) {
+        if (Math.abs(a[i][0] - b[i][0]) > threshold) {
+            return false
+        }
+        return true
+    }
+}
+
+function authenticate(a, b) {
+    // we are now coupling the threshold with the function definition to minimize exports
+    const backBeatThreshold = 500;
+    // first test: are the two arrays the same length:
+    let sameLength = compareLength(a, b);
+    if (sameLength === false) {
         return false
     }
+    let sameNotes = compareNotes(a, b)
+    if (sameNotes === false) {
+        return false
+    }
+    let sameRhythm = compareRhythm(a, b)
+    if (sameRhythm === false) {
+        return false
+    }
+    return true
 }
 
-module.exports = {
-    backBeatThreshold,
-    authenticate
-}
+// frontend-backend switcharoo
+export default authenticate
